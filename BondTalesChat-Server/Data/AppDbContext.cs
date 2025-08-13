00e13bfad1519e7be69fc5e86fcfbe782be5943e
AKASH S <akash.settukannu@claysys.net>
2025-08-13 11:31:35 +0530

    project setup

---
+++ BondTalesChat-Server/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BondTalesChat_Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSets here if using EF Core for tables, otherwise omit
+        // Example:
+        // public DbSet<User> Users { get; set; }
+        // public DbSet<Message> Messages { get; set; }
+    }
+}
--- BondTalesChat-Server/Program.cs
+++ BondTalesChat-Server/Program.cs
@@ -1,7 +1,14 @@
+using BondTalesChat_Server.Data;
+using BondTalesChat_Server.Hubs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,5 +28,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapHub<ChatHub>("/chatHub");
 app.Run();
