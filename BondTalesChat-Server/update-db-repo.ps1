8535a1bdacd5f793adf17fc10da601e8f850a657
AKASH S <akash.settukannu@claysys.net>
2025-08-13 11:15:08 +0530

    project setup

---
+++ BondTalesChat-Server/update-db-repo.ps1
@@ -0,0 +1,11 @@
+﻿$repoUrl = "https://github.com/AKASH-kce/BondTalesChat-Database"
+$targetDir = "BondTalesChat-Database"
+
+if (Test-Path $targetDir) {
+    Write-Output "Repo exists. Pulling latest changes..."
+    Set-Location $targetDir
+    git pull origin main
+} else {
+    Write-Output "Cloning repo..."
+    git clone $repoUrl $targetDir
+}
