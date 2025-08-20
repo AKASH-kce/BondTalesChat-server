966dbfea68198a3ee61a5ee823b662483a18114d
AKASH S <akash.settukannu@claysys.net>
2025-08-20 12:15:07 +0530

    Update Program.cs

--- BondTalesChat-Server/Program.cs
+++ BondTalesChat-Server/Program.cs
@@ -60,6 +60,7 @@ builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddScoped<IConversationService, ConversationService>();
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
